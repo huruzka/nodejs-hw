18e8953d5283e3ee2673b91124a4f76d8a986343
Marina Prushak <87096097+huruzka@users.noreply.github.com>
2025-11-27 19:15:57 +0200

    folder name updated

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { celebrate } from "celebrate";
-import { loginUser, logoutUser, registerUser } from "../controllers/authoController.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/authController.js";
 import { loginUserSchema, registerUserSchema } from "../validations/authValidation.js";
 
 const router = Router();
