357187063c0f879bb519b77b945ffe3c24bad52d
Marina Prushak <87096097+huruzka@users.noreply.github.com>
2025-12-04 20:08:38 +0200

    hw correction

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { authenticate } from '../middleware/authenticate.js';
-import { updateUserAvatar } from "../controllers/userConroller.js";
+import { updateUserAvatar } from "../controllers/userController.js";
 import { upload } from "../middleware/multer.js";
 
 const router = Router();
